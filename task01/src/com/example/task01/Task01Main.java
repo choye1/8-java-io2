b08e187755a8a3ad356f1e0dc3657b39f32c6fbc
choye1 <makcim233237@gmail.com>
2024-11-11 22:23:52 +0500

    task1

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -1,20 +1,35 @@
 package com.example.task01;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
 
-        /*
+
         System.out.println(extractSoundName(new File("task01/src/main/resources/3727.mp3")));
-        */
+
     }
 
     public static String extractSoundName(File file) throws IOException, InterruptedException {
-        // your implementation here
-        return "sound name";
+        ProcessBuilder pb = new ProcessBuilder();
+        pb.command("cmd.exe", "/c", "ffprobe -v error -of flat -show_format " + file.getAbsolutePath())
+                .directory(new File("C:\\Program Files\\ffmpeg\\bin"))
+                .redirectErrorStream(true);
+        Process p = pb.start();
+        InputStream iS = p.getInputStream();
+        try (BufferedReader bR = new BufferedReader(new InputStreamReader(iS))) {
+            String str = bR.readLine();
+            while (str != null) {
+                if (str.contains("title")) {
+                    return str.split("=")[1].replace("\"", "");
+                }
+                str = bR.readLine();
+            }
+
+            return null;
+
+        }
     }
 }
